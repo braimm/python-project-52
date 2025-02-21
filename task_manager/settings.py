f673b8bc8e3239df0426e37c0e8ea241f46371ba
Dmitriy G <llogginn@gmail.com>
2025-02-21 18:33:40 +0500

    fix dev mode

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = False
+# DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', '.onrender.com'] 
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', 'onrender.com'] 
 
 
 # Application definition
